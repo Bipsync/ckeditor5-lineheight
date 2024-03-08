58459f2dc9cac203e29ba07ff4df8cb1e9e337cc
Steve Halford <steve@bipsync.com>
2024-03-08 12:45:56 +0000

    Initialize the repository using CKEditor 5 Package Generator.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ckeditor from './../theme/icons/ckeditor.svg';
+
+export { default as LineHeight } from './lineheight';
+
+export const icons = {
+	ckeditor
+};
---
+++ tests/index.js
@@ -0,0 +1,16 @@
+import { LineHeight as LineHeightDll, icons } from '../src';
+import LineHeight from '../src/lineheight';
+
+import ckeditor from './../theme/icons/ckeditor.svg';
+
+describe( 'CKEditor5 LineHeight DLL', () => {
+	it( 'exports LineHeight', () => {
+		expect( LineHeightDll ).to.equal( LineHeight );
+	} );
+
+	describe( 'icons', () => {
+		it( 'exports the "ckeditor" icon', () => {
+			expect( icons.ckeditor ).to.equal( ckeditor );
+		} );
+	} );
+} );
